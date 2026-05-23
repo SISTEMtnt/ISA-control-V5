7db6c5019491f4541282f476ce1288f7162a32ae
SISTEMtnt <andreatnt12@hotmail.com>
2026-05-23 22:00:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow, screen, ipcMain } = require("electron");
 
 let win;
+let overlayEnabled = true;
 
 function createWindow() {
-
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   win = new BrowserWindow({
@@ -29,13 +29,45 @@ function createWindow() {
 
   win.loadFile("client/index.html");
 
-  // click-through by default
+  // start in click-through mode
   win.setIgnoreMouseEvents(true, { forward: true });
+
+  win.setMenu(null);
 }
 
+/* =========================
+   TOGGLE OVERLAY MODE
+========================= */
+
+ipcMain.on("toggle-overlay", () => {
+  overlayEnabled = !overlayEnabled;
+
+  if (overlayEnabled) {
+    // interactive mode
+    win.setIgnoreMouseEvents(false);
+    win.webContents.send("overlay-state", true);
+  } else {
+    // click-through mode
+    win.setIgnoreMouseEvents(true, { forward: true });
+    win.webContents.send("overlay-state", false);
+  }
+});
+
+/* =========================
+   ABORT APP
+========================= */
+
 ipcMain.on("abort", () => {
+  if (win) win.webContents.send("abort");
   app.quit();
 });
 
+/* =========================
+   APP LIFECYCLE
+========================= */
+
 app.whenReady().then(createWindow);
-app.on("window-all-closed", () => app.quit());
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
